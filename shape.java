ea34a73a2b74897d641cea2999fc1cf93597d4c7
Amil P A <117823429+amilpa@users.noreply.github.com>
2022-11-16 15:44:26 +0530

    committ

--- shape.java
+++ shape.java
@@ -1,13 +1,54 @@
 import java.io.* ; 
 import java.util.* ;
 
+import javax.sound.sampled.SourceDataLine;
 
-abstract class shape
+
+abstract class Shape
 {
-    abstract int numberofsides();
+    abstract void numberofsides();
 }
 
-class rectangle extends shape
+class Rectangle extends Shape
 {
-    private static int sides = 4;
+    int sides = 4;
+    void numberofsides()
+    {
+        System.out.println("Rectangle");
+        System.out.println("Number of sides: "+sides);
+    }
+}
+
+
+class Triangle extends Shape
+{
+    int sides = 3;
+    void numberofsides()
+    {
+        System.out.println("Triangle");
+        System.out.println("Number of sides: "+sides);
+    }
+}
+
+class Hexagon extends Shape
+{
+    int sides = 6;
+    void numberofsides()
+    {
+        System.out.println("Hexagon");
+        System.out.println("Number of sides: "+sides);
+    }
+}
+
+class shape
+{
+    public static void main(String args[])
+    {
+        Rectangle rect = new Rectangle();        
+        rect.numberofsides();
+        Triangle tri = new Triangle();        
+        tri.numberofsides();
+        Hexagon hex = new Hexagon();        
+        hex.numberofsides();
+    }
 }
